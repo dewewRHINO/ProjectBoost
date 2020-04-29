1d9bed5a21eae8c63e40b606fb21372a63618f04
dewewRHINO <jaymerjacob@gmail.com>
2020-04-28 21:12:51 -0700

    Adding Sound.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,22 +7,34 @@ public class Rocket : MonoBehaviour
     // Start is called before the first frame update
 
     Rigidbody rigidBody;
+    AudioSource m_MyAudioSource;
+
+    //Play the music
+    bool m_Play;
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+
+        //Fetch the AudioSource from the GameObject
+        m_MyAudioSource = GetComponent<AudioSource>();
+        //Ensure the toggle is set to true for the music to play at start-up
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
-
-        
     }
     private void ProcessInput(){
         if(Input.GetKey(KeyCode.Space)){
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!m_MyAudioSource.isPlaying)
+            {
+                m_MyAudioSource.Play();
+            }
         }
+        else{ m_MyAudioSource.Stop();}
 
         if(Input.GetKey(KeyCode.A)){
             transform.Rotate(Vector3.forward);
